7191bb4798374e44f60e8e40cfd523ee74e5e8b4
eHealth Africa CI <ehealthafrica-ci@tlvince.com>
2015-05-13 13:09:37 +0000

    v1.1.0

--- README.md
+++ README.md
@@ -12,6 +12,46 @@ This project is a JavaScript implementation of a QSL parser.
 [travis-image]: https://img.shields.io/travis/eHealthAfrica/qsl.js.svg
 [travis-url]: https://travis-ci.org/eHealthAfrica/qsl.js
 
+## Usage
+
+`qsl.js` exposes a single function `parse` the takes a raw QSL document and
+a section name and returns a formatted representation of the given section.
+
+```yml
+# bands.qsl
+Favourite bands survey
+defaults
+  required
+section execution order
+  bands
+bands:
+  First gig?
+  How many gigs?
+    integer
+```
+
+```js
+'use strict';
+
+var qsl = require('qsl');
+qsl.parse(bandsQSL, 'bands');
+// =>
+// [
+//   {
+//     label: 'First gig?',
+//     name: 'first-gig',
+//     type: 'text',
+//     required: true
+//   },
+//   {
+//     label: 'How many gigs?',
+//     name: 'how-many-gigs',
+//     type: 'number',
+//     required: true
+//   }
+// ];
+```
+
 ## Contributors
 
 * Johannes Wilm <http://www.johanneswilm.org>
--- qsl.js
+++ qsl.js
@@ -1,6 +1,147 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.qsl = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var slug = require('to-slug-case');
+
+var types = require('./types');
+
+function isInclusion(row) {
+  return row.key.charAt(0) === '['
+    && row.key.charAt(row.key.length - 1) === ']';
+}
+
+function getInclusion(row) {
+  return row.key.slice(1, -1);
+}
+
+function knownType(row) {
+  if (!row.children.length) {
+    // Default to 'text' type, sans constraints
+    row.children = [
+      {
+        key: 'text',
+        children: []
+      }
+    ];
+  }
+  return row.children[0].key in types;
+}
+
+function collateEnumerations(enumeable) {
+  function collate(enumerate) {
+    return {
+      label: enumerate.key,
+      value: slug(enumerate.key)
+    };
+  }
+  return enumeable.map(collate);
+}
+
+function appendDefaults(defaults, section) {
+  // Currently, only the `required` default can be set
+  // https://github.com/eHealthAfrica/qsl#the-defaults-section
+  var required = defaults.children[0];
+  if (!(required && required.key && required.key === 'required')) {
+    return section;
+  }
+  section.required = true;
+  return section;
+}
+
+function pluckSection(sections, section) {
+  // Questionnaire sections have a colon suffix
+  section += ':';
+  section = sections[section];
+  if (!section) {
+    return {};
+  }
+  return section;
+}
+
+function indexByKey(qsl) {
+  function byKey(accumilator, section) {
+    if (!section.key) {
+      return accumilator;
+    }
+    accumilator[section.key] = section;
+    return accumilator;
+  }
+  return qsl.reduce(byKey, {});
+}
+
+function parseSection(index, section) {
+  section = pluckSection(index, section);
+  if (index.defaults) {
+    section = appendDefaults(index.defaults, section);
+  }
+
+  if (!(section && section.children)) {
+    return [];
+  }
+
+  var elements = [];
+
+  function pushInclusion(inclusion) {
+    elements.push(inclusion);
+  }
+
+  function createElement(row) {
+    if (isInclusion(row)) {
+      var inclusion = getInclusion(row);
+      var inclusions = parseSection(index, inclusion);
+      inclusions.forEach(pushInclusion);
+      return;
+    }
+
+    var element = {
+      label: row.key,
+      name: slug(row.key)
+    };
+
+    var key = row.children[0].key;
+    var map = types[key];
+
+    element.type = map.type;
+    if (map.options) {
+      element.fields = map.options;
+    }
+
+    if (section.required) {
+      element.required = true;
+    }
+
+    var children = row.children[0].children;
+    if (!children.length) {
+      elements.push(element);
+      return;
+    }
+
+    if (key === 'enumeration') {
+      element.fields = collateEnumerations(children);
+    }
+
+    if (children[0].key && children[0].key === 'optional') {
+      element.required = false;
+    }
+
+    elements.push(element);
+  }
+
+  section.children
+    .filter(knownType)
+    .forEach(createElement);
+
+  return elements;
+}
+
+exports.format = function(qsl, section) {
+  var index = indexByKey(qsl);
+  return parseSection(index, section);
+};
+
+},{"./types":3,"to-slug-case":4}],2:[function(require,module,exports){
+'use strict';
+
 exports.parse = function(rawQSL) {
   var rawLines = rawQSL.split('\n');
   var cleanedLines = [];
@@ -63,10 +204,201 @@ exports.parse = function(rawQSL) {
   return parsedLines;
 };
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 'use strict';
 
-module.exports = require('./lib/qsl');
+/**
+ * Maps QSL data types with templates containing appropriate HTML form elements.
+ */
+module.exports = {
+  text: {
+    type: 'text'
+  },
+  integer: {
+    type: 'number',
+    step: 1,
+    min: 0
+  },
+  real: {
+    type: 'number'
+  },
+  date: {
+    type: 'date'
+  },
+  time: {
+    type: 'time'
+  },
+  datetime: {
+    type: 'datetime'
+  },
+  gps: {
+    type: 'number'
+  },
+  enumeration: {
+    type: 'radio',
+    options: []
+  },
+  'yes/no': {
+    type: 'yes/no',
+    options: [
+      {
+        label: 'Yes',
+        value: 'Y'
+      },
+      {
+        label: 'No',
+        value: 'N'
+      }
+    ]
+  },
+  gender: {
+    type: 'gender',
+    options: [
+      {
+        label: 'Male',
+        value: 'M'
+      },
+      {
+        label: 'Female',
+        value: 'F'
+      }
+    ]
+  }
+};
 
-},{"./lib/qsl":1}]},{},[2])(2)
+},{}],4:[function(require,module,exports){
+
+var toSpace = require('to-space-case');
+
+
+/**
+ * Expose `toSlugCase`.
+ */
+
+module.exports = toSlugCase;
+
+
+/**
+ * Convert a `string` to slug case.
+ *
+ * @param {String} string
+ * @return {String}
+ */
+
+
+function toSlugCase (string) {
+  return toSpace(string).replace(/\s/g, '-');
+}
+},{"to-space-case":5}],5:[function(require,module,exports){
+
+var clean = require('to-no-case');
+
+
+/**
+ * Expose `toSpaceCase`.
+ */
+
+module.exports = toSpaceCase;
+
+
+/**
+ * Convert a `string` to space case.
+ *
+ * @param {String} string
+ * @return {String}
+ */
+
+
+function toSpaceCase (string) {
+  return clean(string).replace(/[\W_]+(.|$)/g, function (matches, match) {
+    return match ? ' ' + match : '';
+  });
+}
+},{"to-no-case":6}],6:[function(require,module,exports){
+
+/**
+ * Expose `toNoCase`.
+ */
+
+module.exports = toNoCase;
+
+
+/**
+ * Test whether a string is camel-case.
+ */
+
+var hasSpace = /\s/;
+var hasCamel = /[a-z][A-Z]/;
+var hasSeparator = /[\W_]/;
+
+
+/**
+ * Remove any starting case from a `string`, like camel or snake, but keep
+ * spaces and punctuation that may be important otherwise.
+ *
+ * @param {String} string
+ * @return {String}
+ */
+
+function toNoCase (string) {
+  if (hasSpace.test(string)) return string.toLowerCase();
+
+  if (hasSeparator.test(string)) string = unseparate(string);
+  if (hasCamel.test(string)) string = uncamelize(string);
+  return string.toLowerCase();
+}
+
+
+/**
+ * Separator splitter.
+ */
+
+var separatorSplitter = /[\W_]+(.|$)/g;
+
+
+/**
+ * Un-separate a `string`.
+ *
+ * @param {String} string
+ * @return {String}
+ */
+
+function unseparate (string) {
+  return string.replace(separatorSplitter, function (m, next) {
+    return next ? ' ' + next : '';
+  });
+}
+
+
+/**
+ * Camelcase splitter.
+ */
+
+var camelSplitter = /(.)([A-Z]+)/g;
+
+
+/**
+ * Un-camelcase a `string`.
+ *
+ * @param {String} string
+ * @return {String}
+ */
+
+function uncamelize (string) {
+  return string.replace(camelSplitter, function (m, previous, uppers) {
+    return previous + ' ' + uppers.toLowerCase().split('').join(' ');
+  });
+}
+},{}],7:[function(require,module,exports){
+'use strict';
+
+var parser = require('./lib/parse');
+var formatter = require('./lib/format');
+
+exports.parse = function(rawQSL, section) {
+  var qsl = parser.parse(rawQSL);
+  return formatter.format(qsl, section);
+};
+
+},{"./lib/format":1,"./lib/parse":2}]},{},[7])(7)
 });
